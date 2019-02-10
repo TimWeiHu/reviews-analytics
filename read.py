34c7461ae500451ba1f678899277c165b3dff74d
TimWeiHu <win94717@gmail.com>
2019-02-10 23:08:20 +0800

    add average len part

--- read.py
+++ read.py
@@ -4,10 +4,12 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0:
+        if count % 10000 == 0:
             print(len(data))
-print(len(data))
-print('---------------------------')
-print(data[0])
-print('---------------------------')
-print(data[1])
+print('檔案讀取完成，共有', len(data), '筆資料')
+
+L = 0
+for review in data:
+    L += len(review)
+L_avg = L / len(data)
+print('每筆留言平均長度為：', L_avg)
